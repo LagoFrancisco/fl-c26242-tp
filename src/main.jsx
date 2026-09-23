862f37fda234505cfb17f27e93fb1fc75709bc99
Francisco Lago <vetting.ar@gmail.com>
2026-09-23 20:11:28 -0300

    Cambios en enrutamiento

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/fl-c26242-tp">
       <App />
     </BrowserRouter>
   </StrictMode>,
