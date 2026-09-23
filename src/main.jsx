fe8f64859a1554939ed72d1dda6d27e1bdafaca6
Francisco Lago <vetting.ar@gmail.com>
2026-09-23 20:17:10 -0300

    Retiro de las modificaciones

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/fl-c26242-tp">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
